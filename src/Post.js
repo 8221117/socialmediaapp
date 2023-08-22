13566485a9b5e2930dd861e4e0552abbf2ad26c6
Nandhini S <sainandhu1998@gmail.com>
2023-08-23 00:33:16 +0530

    useparams hook

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Post = () => {
-  return <div>Post</div>;
+  const { id } = useParams();
+  return <div>Post {id}</div>;
 };
 
 export default Post;
